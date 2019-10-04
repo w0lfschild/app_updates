dec9e9d4c134512d5a63a25d1dec2fdef50abdd2
w0lfschild <aguywithlonghair@yahoo.com>
2019-10-03 19:27:22 -0700

    Updated MacForge

---
+++ MacForge/_publish.sh
@@ -0,0 +1,18 @@
+#! /bin/bash
+
+_path=$(dirname "$0")
+_file="${_path##*/}"
+
+# Update appcast
+"$_path"/bin/generate_appcast "/Users/w0lf/Library/Mobile Documents/com~apple~CloudDocs/dsa_priv.pem" "$_path"
+
+# Push to GitHub
+cd "$_path"
+cd ../
+echo "$PWD"
+git pull
+git add .
+git commit -m "Updated $_file"
+git push
+
+# END

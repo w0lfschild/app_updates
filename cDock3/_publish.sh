d8454968b145a945b74d84fc57bf1a52fa753e6c
Wolfgang Baird <aguywithlonghair@yahoo.com>
2020-03-17 02:15:27 -0700

    Updated cDock3

--- cDock3/_publish.sh
+++ cDock3/_publish.sh
@@ -3,8 +3,7 @@
 _path=$(dirname "$0")
 _root=$(dirname $_path)
 _file="${_path##*/}"
-_hdir="~/Library/Mobile Documents/com~apple~CloudDocs/dsa_priv.pem"
-eval _hdir=$_hdir
+_hdir="$HOME/Library/Mobile Documents/com~apple~CloudDocs/dsa_priv.pem"
 
 # Update appcast
 "$_root"/bin/generate_appcast "$_hdir" "$_path"
